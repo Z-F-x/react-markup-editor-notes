288d5c807c276b488ed8581a6f7f940c161c2bdc
benjamin-roedstien <123411327+benjamin-roedstien@users.noreply.github.com>
2023-09-19 18:16:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-// vite.config.js
 export default defineConfig({
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.js',
-    },
-  },
+	plugins: [react()]	
 })
