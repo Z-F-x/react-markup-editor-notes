720470ec9b4da083f3d478626780a065b1f189c3
benjamin-roedstien <123411327+benjamin-roedstien@users.noreply.github.com>
2023-09-19 18:30:12 +0200

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+import 'vite/modulepreload-polyfill'
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
+
